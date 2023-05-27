47b3b72b17b57baeced96808c12d95f5fcf5b844
YidnekachewTefera <yomaytec@gmail.com>
2023-05-27 06:51:47 -0700

    completed the UI

---
+++ calculator.js
@@ -0,0 +1,13 @@
+const result = document.querySelector(".result");
+const currentOperandsAndOperater = document.querySelector(
+  ".current-operands-and-operater"
+);
+const numbers = document.querySelectorAll("button");
+console.log(numbers);
+currentOperandsAndOperater.textContent = 0;
+result.textContent = 0;
+console.log(result);
+
+let initial_value1 = 0;
+let initial_value2 = 0;
+const sumation = (a, b) => {};
